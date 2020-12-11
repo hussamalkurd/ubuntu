08b4ab188551c6109ea54451bd1884c065a93a9d
Hussam Al Kurd <hussam-kurd@hotmail.com>
2020-12-11 21:37:30 +0200

    Update ubuntu.bash

--- ubuntu.bash
+++ ubuntu.bash
@@ -1,5 +1,5 @@
 user@host:/home/user# ls -l /etc/shadow
--rw-r-----  1 root shadow 869 2005-11-08 13:16 /etc/shadow
+-rw-r-----  1 root code.py 869 2005-11-08 13:16 /etc/shadow
 user@host:/home/user#
 
 Permissions:
