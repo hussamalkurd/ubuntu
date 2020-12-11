09cee1a1a909b9a8457cc3876e1901e955211924
Hussam Al Kurd <hussam-kurd@hotmail.com>
2020-12-11 21:15:07 +0200

    Update ubuntu.bash

--- ubuntu.bash
+++ ubuntu.bash
@@ -1,1 +1,13 @@
 
+user@host:/home/user# ls -l /etc/shadow
+-rw-r-----  1 root shadow 869 2005-11-08 13:16 /etc/shadow
+user@host:/home/user#
+
+Permissions:
+owner = Read & Write (rw-)
+group = Read (r--)
+other = None (---)
+
+Ownership:
+owner = root
+group = shadow
